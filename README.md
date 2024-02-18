8b8f1d2186f9f8d9809a52bee241b108fac34aa6
MidnightLunaCassie123 <127367938+MidnightLunaCassie123@users.noreply.github.com>
2024-02-17 19:50:49 -0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# pro-85
+# spectagram-stage-1
+project solution for c81
---
+++ config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAlgCBxu_pkOeb-447_8EHE9J_zEgz7N2E",
+    authDomain: "spectogramapp-8c50e.firebaseapp.com",
+    projectId: "spectogramapp-8c50e",
+    storageBucket: "spectogramapp-8c50e.appspot.com",
+    messagingSenderId: "82386796754",
+    appId: "1:82386796754:web:d683def4a5f4c3728ead0b"
+  };
